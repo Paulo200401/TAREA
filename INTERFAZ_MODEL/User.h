c0cdda4c122f94439ed204890dfa570397acfa3e
MitsukiYas <myasuda2004@gmail.com>
2024-02-16 12:42:44 -0500

    xd

---
+++ INTERFAZ_MODEL/User.h
@@ -0,0 +1,15 @@
+/**
+ * Project Untitled
+ */
+
+using namespace System;
+#pragma once
+
+namespace  INTERFAZ_MODEL {
+    public ref class User {
+    public:
+        String^ name;
+        String^ user;
+        String^ password;
+    };
+}
