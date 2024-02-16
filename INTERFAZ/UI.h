519194180a21ec9ce635004f46118c0405f0a665
Paulo200401 <a20211880@pucp.edu.pe>
2024-02-16 12:14:19 -0500

    cambios

--- INTERFAZ/UI.h
+++ INTERFAZ/UI.h
@@ -34,6 +34,16 @@ namespace INTERFAZ {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+
 
 	private:
 		/// <summary>
@@ -48,12 +58,107 @@ namespace INTERFAZ {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"UI";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(UI::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(62, 353);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(125, 54);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Ingresar";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &UI::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(229, 353);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(125, 54);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Crear";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(68, 135);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(96, 29);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Usuario";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(28, 202);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(136, 29);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Contraseña";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(192, 135);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(162, 22);
+			this->textBox1->TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(192, 209);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(162, 22);
+			this->textBox2->TabIndex = 5;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(400, 54);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(303, 294);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 6;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &UI::pictureBox1_Click);
+			// 
+			// UI
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(731, 428);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Name = L"UI";
+			this->Text = L"UI";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	
+private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
